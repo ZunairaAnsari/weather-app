b2181578dbe70bbd225da2f928e0c6ca798041e8
Zunaira Fawwad <binteajazansari@gmail.com>
2024-02-18 19:40:29 +0500

    reactApi working in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { TiWeatherCloudy } from "react-icons/ti";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('Karachi');
+  const [cityName, setCityName] = useState("");
+  const [result, setResult] = useState({})
+  const API = {
+    key : 'f0adf1349da0edbee050d79c6def2456',
+    url : 'https://api.openweathermap.org/data/2.5/weather'
+  }
+
+  const handleClick = () => {
+    setCity(cityName)
+  }
+  useEffect(() => {
+    fetch(`${API.url}?q=${city}&appid=${API.key}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setResult(data);
+    })
+  }, [,city])
+//  console.log(result);
+
+
+  // console.log(result.weather[0].description);
+  // const {name , main} = result
+  // const {temp} = main
+  // console.log(name, main)
+  // console.log(temp)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <div>
+      <input type="text" placeholder='' id='' name='' onChange={(e) => {setCityName(e.target.value)}} />
+      <button onClick={handleClick}>Search</button>
+      </div>
+      <h2>{result.main && result.main.temp}°F</h2>
+      <h1 ><TiWeatherCloudy style={{color : "skyblue"}}/>{result.main && result.weather[0].description}</h1>
     </div>
+
   );
 }
 
