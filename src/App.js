52ed29f7f35eaa16f9be29b0af48fa39463af20b
ZunairaAnsari <zunairaansari413@gmail.com>
2024-02-26 12:47:44 +0500

    Completed weather app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Weather from './components/Weather';
-import axios from 'axios';
 
 function App() {
  return (
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from 'react';
 import { TiWeatherCloudy } from "react-icons/ti";
+import { FaSearch } from "react-icons/fa";
+
+
 const Weather = () => {
+  const weatherIcons = {
+    Clear: 'sun',
+    Clouds: 'cloud',
+    Smoke: 'cloud-showers-heavy',
+
+  };
   const [city, setCity] = useState('Karachi');
   const [cityName, setCityName] = useState("");
   const [result, setResult] = useState({})
@@ -9,11 +18,19 @@ const Weather = () => {
     url: 'https://api.openweathermap.org/data/2.5/weather'
   }
 
-  const handleClick = () => {
-    setCity(cityName)
-  }
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const handleSearch = () => {
+    setCity(cityName);
+    setCityName("");
+  };
+
   useEffect(() => {
-    fetch(`${API.url}?q=${city}&appid=${API.key}`)
+    fetch(`${API.url}?q=${city}&appid=${API.key}&units=metric`)
       .then(res => res.json())
       .then(data => {
         console.log(data);
@@ -29,33 +46,57 @@ const Weather = () => {
   // console.log(name, main)
   // console.log(temp)
   return (
-    <>
+    <><h2 className='heading'>Weather ForeCast</h2>
       <div className='container'>
+      <div className='search-bar'>
+          <input
+            type="text"
+            placeholder='Enter city...'
+            style={{ color: 'white' }}
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            onKeyDown={handleKeyPress}
+          />
+          <span className='icon'>
+          <FaSearch />
+          </span>
+        </div>
         <div className='top'>
-          <div className='location'><p>dallas</p></div>
-          <div className='temperature'><h1>54</h1></div>
-          <div className='description'>aaaa</div>
-        </div>
-        <div className='bottom'>
-          <div className='feels'>
-            <p className='bold'>65 °F</p>
-            <p>Feels Like</p>
+          <div className='location'><p>{result.name}</p></div>
+          <div className='temperature'>{
+            result.main ? <h1>{result.main.temp.toFixed()}°C</h1> : null
+          }</div>
+          <div className='description'>
+            {result.weather && (
+              <>
+                <TiWeatherCloudy icon={weatherIcons[result.weather[0].main]} />
+                <p>{result.weather[0].description}</p>
+              </>
+            )}
           </div>
-          <div className='humidity'>
-            <p className='bold'>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            <p className='bold'>12MPH</p>
-            <p>Wind Speed</p>
-          </div>
+          {/* <div className='description'>{result.main && result.weather[0].description}</div> */}
         </div>
-        <div>
-          <input type="text" placeholder='' id='' name='' onChange={(e) => { setCityName(e.target.value) }} />
-          <button onClick={handleClick}>Search</button>
-        </div>
-        <h2>{result.main && result.main.temp}°F</h2>
-        <h1 ><TiWeatherCloudy style={{ color: "skyblue" }} />{result.main && result.weather[0].description}</h1>
+
+        {result.name !== undefined &&
+          <div className='bottom'>
+            {result.main && result.weather && (
+              <>
+                <div className='feels'>
+                  <p className='bold'>{result.main.feels_like.toFixed()} °C</p>
+                  <p>Feels Like</p>
+                </div>
+                <div className='humidity'>
+                  <p className='bold'>{result.main.humidity.toFixed()}%</p>
+                  <p>Humidity</p>
+                </div>
+                <div className='wind'>
+                  <p className='bold'>{result.wind.speed.toFixed()} MPH</p>
+                  <p>Wind Speed</p>
+                </div>
+              </>
+            )}
+          </div>}
+        <div></div>
       </div>
     </>
   );
